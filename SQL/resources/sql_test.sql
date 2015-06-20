becf903fc7ef8a5048d78f4019e9fe22a1a2ad95
IrinaPeresunko <peresunkoi@gmail.com>
2015-06-20 23:24:08 +0300

    connection to the database "users"

---
+++ SQL/resources/sql_test.sql
@@ -0,0 +1,7 @@
+--update users set name='Steve',age=55 where id=1;
+--select * from users;
+--select name,age from users where name='Steve';
+--update users set name='Steve',age=55 where id=1;
+--delete from users where id=4;
+--select * from users where name='Alex';
+insert into users (name,age,email) values('Steve',22,'steve@devcolibri.com');
--- SQL/src/createDatabase/CreateDatabase.java
+++ SQL/src/createDatabase/CreateDatabase.java
@@ -1,53 +1,21 @@
 package createDatabase;
+
 import java.sql.*;
 
 public class CreateDatabase {
-   // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/";
-
-   //  Database credentials
-   static final String USER = "username";
-   static final String PASS = "password";
-   
-   public static void main(String[] args) {
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //STEP 2: Register JDBC driver
-      Class.forName("com.mysql.jdbc.Driver");
-
-      //STEP 3: Open a connection
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-      //STEP 4: Execute a query
-      System.out.println("Creating database...");
-      stmt = conn.createStatement();
-      
-      String sql = "CREATE DATABASE STUDENTS";
-      stmt.executeUpdate(sql);
-      System.out.println("Database created successfully...");
-   }catch(SQLException se){
-      //Handle errors for JDBC
-      se.printStackTrace();
-   }catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
-   }finally{
-      //finally block used to close resources
-      try{
-         if(stmt!=null)
-            stmt.close();
-      }catch(SQLException se2){
-      }// nothing we can do
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }//end finally try
-   }//end try
-   System.out.println("Goodbye!");
-}//end main
-}//end JDBCExample
+	public static void main(String[] args){
+		try{
+			//Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydbtest", "root", "password");
+			String guery = "Select * FROM users";
+			Statement stm = conn.createStatement();
+			ResultSet rs = stm.executeQuery(guery);
+			while(rs.next()){
+				System.out.println(rs.getString("id")+" " + rs.getString("name")+ " "+rs.getString("age"));
+			}
+		//	System.out.println("good");
+		}catch(Exception e){
+			System.err.println(e);
+		}
+	}	
+}
