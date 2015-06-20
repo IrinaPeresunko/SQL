47d086095cbcc045e7e27316123a1704a493a8a7
IrinaPeresunko <peresunkoi@gmail.com>
2015-06-21 00:22:46 +0300

    execute query and getting result in the result set

--- SQL/resources/sql_test.sql
+++ SQL/resources/sql_test.sql
@@ -1,7 +1,7 @@
+select * from users;
 --update users set name='Steve',age=55 where id=1;
---select * from users;
 --select name,age from users where name='Steve';
 --update users set name='Steve',age=55 where id=1;
 --delete from users where id=4;
 --select * from users where name='Alex';
-insert into users (name,age,email) values('Steve',22,'steve@devcolibri.com');
+--insert into users (name,age,email) values('Steve',22,'steve@devcolibri.com');
--- SQL/src/createDatabase/CreateDatabase.java
+++ SQL/src/createDatabase/CreateDatabase.java
@@ -3,19 +3,50 @@ package createDatabase;
 import java.sql.*;
 
 public class CreateDatabase {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+	private static String NAME = "root";
+	private static String PASSWORD = "password";
+	private static String query;
+	private static Statement statement;
+	private static Connection connection;
+	
+	public static void print(int id,String name, int age, String email){
+		System.out.println("\t"+"|id:"+id+" | name:"+name+" | age:"+age+" | email:"+email);
+	}
 	public static void main(String[] args){
 		try{
-			//Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydbtest", "root", "password");
-			String guery = "Select * FROM users";
-			Statement stm = conn.createStatement();
-			ResultSet rs = stm.executeQuery(guery);
-			while(rs.next()){
-				System.out.println(rs.getString("id")+" " + rs.getString("name")+ " "+rs.getString("age"));
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL,NAME,PASSWORD);
+			System.out.println("Connected");
+			
+			query = "Select * FROM users WHERE age>20 AND age<45";
+			statement = connection.createStatement();
+			
+			ResultSet resultSet = statement.executeQuery(query);
+			while(resultSet.next()){
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				int age = resultSet.getInt("age");
+				String email = resultSet.getString("email");
+				print(id,name,age,email);
 			}
-		//	System.out.println("good");
+			
+			connection.close();
 		}catch(Exception e){
 			System.err.println(e);
+		}
+		finally{
+			if(statement!=null){
+				try{
+					statement.close();
+				}catch(Exception e){System.err.println(e);}
+			}
+			if(connection!=null){
+				try{
+					connection.close();
+				}catch(Exception e){System.err.println(e);}
+			}
 		}
 	}	
 }
